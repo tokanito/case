1589df9370c2345943d248856985333a079cc0b7
tokanito <78441278+tokanito@users.noreply.github.com>
2021-04-01 22:22:41 +0400

    fix:
    smth

--- case/script.js
+++ case/script.js
@@ -1,41 +1,39 @@
 var completed =false;
 const confirmed = document.querySelector ('.btn');
-const lastName = document.querySelector ('.feedback--input__lastname');
-const firstName = document.querySelector ('.feedback--input__name');
-const birth = document.querySelector ('.feedback--input__date');
-const phoneNumber = document.querySelector ('.feedback--input__number');
-const group = document.querySelector ('.feedback--input__group');
-const city = document.querySelector ('.address--input__city');
-const documentType = document.querySelector ('.passport-form');
-const documentDate = document.querySelector ('.passport-form--option__date');
-
-
-let arr = [confirmed,lastName,firstName,birth,phoneNumber,group,city,documentType,documentDate]
-for (let i=0; i<arr.length; i++){
-    console.log (arr[i]);
-}
 
 
 
 
+
+let clientNumber =0;
 
 
 confirmed.addEventListener ('click', function(){
+    // clientNumber++;
+    // var 
+    
     const lastName = document.querySelector ('.feedback--input__lastname').value;
     const firstName = document.querySelector ('.feedback--input__name').value;
-    const birth = document.querySelector ('.feedback--input__date');
+    const birth = document.querySelector ('.feedback--input__date').value;
     const phoneNumber = document.querySelector ('.feedback--input__number').value;
-    const group = document.querySelector ('.feedback--input__group');
+    const group = document.querySelector ('.feedback--input__group').value;
     const city = document.querySelector ('.address--input__city').value;
-    const documentType = document.querySelector ('.passport-form');
-    const documentDate = document.querySelector ('.passport-form--option__date');
+    const documentType = document.querySelector ('.passport-form--option__type').value;
+    const documentDate = document.querySelector ('.passport-form--option__date').value;
 
 
     const confirmForm=document.querySelector(".img-confirm");
     confirmForm.innerHTML = "Форма успешно отправлена";
-    var val = document.getElementById('lastname').value;
-    var val1 = document.querySelector ('.feedback--input__lastname').value;
-    console.log (val);
-    console.log (val1);
+   
+    let arr = [lastName,firstName,birth,phoneNumber,group,city,documentType,documentDate];
+    var k=0;
+    for (let i=0; i<arr.length; i++){
+        console.log (typeof  arr[i]);
+        if ((arr[i]!==undefined) && (arr[i]!=='false')){
+            k++;
+        
+        }
+    }
+    console.log (k);
     
 })
